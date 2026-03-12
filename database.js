d6dd296546905253cee7734042fcf9914b2b30f1
kiokomutunga <kiokomutungapp@gmail.com>
2026-03-13 01:53:33 +0300

    database conn

--- database.js
+++ database.js
@@ -1,5 +1,22 @@
 const mongoose = require (mongoose)
 
-mongoose.connect(process.env.mongourl)
-.then(() => ("database connected"))
-.err(() => ("failed to connect"))
+const connectedDB = async () => {
+    try{
+
+        conn = mongoose.connect(process.env.mongourl)
+
+        console.log("database connected successfully")
+
+    }
+    catch(error){
+        console.log(`could not connect to database: & {error.message} `);
+        process.exit(1) //close the application after fail
+
+    }
+    
+
+    
+
+};
+
+module.exports = connectedDB;
